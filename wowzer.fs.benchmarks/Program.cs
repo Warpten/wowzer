cf27713034ecd399dda4b25985970733420134f1
Warpten <vertozor@gmail.com>
2024-10-31 16:39:41 +0000

    tenative optimization for ckeys & ekeys parsing - preliminary testing shows 33% gain and saved allocations.

--- wowzer.fs.benchmarks/Program.cs
+++ wowzer.fs.benchmarks/Program.cs
@@ -1,4 +1,6 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Text;
+using System.Text.Unicode;
+using BenchmarkDotNet.Running;
 
 using wowzer.fs.benchmarks.Benchmarks;
 
@@ -8,6 +10,14 @@ namespace wowzer.fs.benchmarks
     {
         static void Main(string[] args)
         {
+            var hs = new HexStringParserBenchmark();
+            var dataSource = Enumerable.Single(hs.ArgumentsSource());
+            Console.WriteLine("Source: {0}", string.Join(' ', dataSource.Item2));
+            Console.WriteLine("Source: {0}", dataSource.Item1);
+            Console.WriteLine("Reference: {0}", Convert.ToHexString(hs.Baseline(dataSource)));
+            Console.WriteLine("Vectorized: {0}", Convert.ToHexString(hs.VectorizedBytes(dataSource)));
+
+
             var cb = new EndiannessConversionBenchmarks();
             cb.Setup();
             cb.BenchmarkEndianness();

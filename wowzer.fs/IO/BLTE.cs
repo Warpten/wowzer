e7fec75b523b930155d1c00b5607a382224dbfac
Warpten <vertozor@gmail.com>
2024-11-11 12:04:03 +0100

    CASC: Optimize BLTE seek operations impact on disk I/O.

--- wowzer.fs/IO/BLTE.cs
+++ wowzer.fs/IO/BLTE.cs
@@ -36,13 +36,17 @@ namespace wowzer.fs.IO
 
             _header = new Header(headerSize, flags);
             _chunks = GC.AllocateUninitializedArray<ChunkInfo>(chunkCount);
+
+            var chunkOffset = headerSize;
             for (var i = 0; i < chunkCount; ++i)
             {
                 var compressedSize = _underlyingStream.ReadInt32BE();
                 var decompressedSize = _underlyingStream.ReadInt32BE();
 
                 var checksum = _underlyingStream.ReadUInt128BE();
-                _chunks[i] = new(compressedSize - 1, decompressedSize, checksum, 0, decompressedSize);
+                _chunks[i] = new(compressedSize - 1, decompressedSize, checksum, chunkOffset, 0, decompressedSize);
+
+                chunkOffset += compressedSize;
             }
         }
 
@@ -105,153 +109,68 @@ namespace wowzer.fs.IO
             return totalReadCount;
         }
 
-        // Dear future me: see the comments on SeekCore. This function is broken somewhere.
-        private void SeekCore2(long decompressedOffset)
-        {
-            _position = (int) decompressedOffset;
-
-            var seekOffset = _header.headerSize;
-            var seekOrigin = SeekOrigin.Begin;
-
-            for (var i = 0; i < _chunks.Length && decompressedOffset > 0; ++i)
-            {
-                ref var currentChunk = ref _chunks.UnsafeIndex(i);
-
-                if (currentChunk.CompressionMode == 0)
-                {
-                    _underlyingStream.Seek(seekOffset, seekOrigin);
-                    Initialize(ref currentChunk, true);
-
-                    seekOffset = 0;
-                    seekOrigin = SeekOrigin.Current;
-                }
-                else
-                {
-                    seekOffset += 1;
-                }
-
-                switch (currentChunk.CompressionMode)
-                {
-                    case (byte)'N':
-                        {
-                            var consumableInChunk = (int)Math.Min(decompressedOffset, currentChunk.DecompressedSize);
-                            seekOffset += consumableInChunk;
-
-                            currentChunk.Remainder = currentChunk.DecompressedSize - consumableInChunk;
-                            decompressedOffset -= consumableInChunk;
-                            break;
-                        }
-                    case (byte)'Z':
-                        {
-                            // Encountered Z chunk.
-                            if (decompressedOffset >= currentChunk.DecompressedSize)
-                            {
-                                // Skip right past it
-                                seekOffset += currentChunk.CompressedSize;
-                                decompressedOffset -= currentChunk.DecompressedSize;
-                            }
-                            else
-                            {
-                                // We're in the middle of it.
-                                _underlyingStream.Seek(seekOffset, seekOrigin);
-
-                                var consumableInChunk = (int)Math.Min(decompressedOffset, currentChunk.DecompressedSize);
-                                _currentChunk = new ZLibStream(_underlyingStream.ReadSlice(currentChunk.CompressedSize), CompressionMode.Decompress);
-                                _currentChunk.Skip(consumableInChunk);
-
-                                currentChunk.Remainder = currentChunk.DecompressedSize - consumableInChunk;
-                                decompressedOffset -= consumableInChunk;
-
-                                _chunkIndex = i;
-                                return;
-                            }
-
-                            break;
-                        }
-                    default:
-                        throw new NotImplementedException();
-                }
-
-                if (currentChunk.Remainder == 0)
-                {
-                    _currentChunk?.Dispose();
-                    _chunkIndex = i + 1;
-                }
-            }
-
-            _underlyingStream.Seek(seekOffset, seekOrigin);
-        }
-
-        // Try to optimize the number of Win32 API calls if you can be bothered - past you can't. -Warpten.
-        // Ideally every seek operation (calls to Skip) should be buffered until 'Z' is hit, at which point
-        // either:
-        // 1. All the chunk is skipped, so just increment by CompressedSize + 1
-        // 2. Part of the chunk is skipped, so skip to the start of the chunk, decompress whatever amount is needed, and exit.
-        // 3. Very optional - in case of nested BLTEs, buffer further?
         private long SeekCore(long decompressedOffset)
         {
             if (!CanSeek)
                 throw new NotSupportedException();
 
             _position = 0;
-
-            // Broken if this is not commented but prevents seeking backwards if commented
-            // _underlyingStream.Seek(_header.headerSize, SeekOrigin.Begin);
-
             for (var i = 0; i < _chunks.Length && decompressedOffset > 0; ++i)
             {
                 ref var currentChunk = ref _chunks.UnsafeIndex(i);
-                if (currentChunk.Remainder == 0)
+
+                if (decompressedOffset >= currentChunk.DecompressedSize)
                 {
+                    // Skipping straight over this chunk
+                    currentChunk.Remainder = 0;
+
                     decompressedOffset -= currentChunk.DecompressedSize;
                     _position += currentChunk.DecompressedSize;
 
-                    // Broken if this is not commented but prevents seeking backwards if commented
-                    // _underlyingStream.Skip(currentChunk.CompressedSize + 1);
+                    _currentChunk?.Dispose();
+
+                    // Prepare for next chunk.
+                    _chunkIndex = i + 1;
+                    _underlyingStream.Seek(currentChunk.Offset + currentChunk.CompressedSize + 1, SeekOrigin.Begin);
                 }
                 else
                 {
+                    // Current chunk compression is unknown, seek to it now and read it because
+                    // we need special processing for Z chunks.
                     if (currentChunk.CompressionMode == 0)
-                        Initialize(ref currentChunk);
-
-                    switch (currentChunk.CompressionMode)
                     {
-                        case (byte)'N':
-                        case (byte)'Z':
-                            {
-                                Debug.Assert(_currentChunk != null);
-
-                                var consumableInChunk = (int) Math.Min(decompressedOffset, currentChunk.DecompressedSize);
-
-                                var previousRemainder = currentChunk.Remainder;
-                                currentChunk.Remainder = currentChunk.DecompressedSize - consumableInChunk;
-
-                                _currentChunk!.Skip(previousRemainder - currentChunk.Remainder);
-                                if (currentChunk.Remainder == 0) {
-                                    _currentChunk!.Dispose();
-                                    _currentChunk = null;
-                                }
-
-                                decompressedOffset -= consumableInChunk;
-                                _position += consumableInChunk;
-                                break;
-                            }
-                        default:
-                            throw new NotImplementedException();
+                        _underlyingStream.Seek(currentChunk.Offset, SeekOrigin.Begin);
+                        Initialize(ref currentChunk, true);
                     }
 
-                    if (currentChunk.Remainder == 0)
+                    if (currentChunk.CompressionMode == (byte)'Z')
                     {
-                        _chunkIndex = i + 1;
+                        // Special case for zlibbed chunks; we are stopping in the middle
+                        // but we still need to parse (and discard) the N bytes leading up
+                        // to the stop point.
 
-                        if (i + 1 >= _chunks.Length)
-                            return Position;
+                        // Seek to the start of the chunk's compressed data.
+                        _underlyingStream.Seek(currentChunk.Offset + 1, SeekOrigin.Begin);
 
+                        _currentChunk?.Dispose();
+                        _currentChunk = new ZLibStream(_underlyingStream.ReadSlice(currentChunk.CompressedSize), CompressionMode.Decompress);
+                        _currentChunk.Skip((int)decompressedOffset);
                     }
+                    else if (currentChunk.CompressionMode == (byte)'N')
+                    {
+                        // Skip to the actual offset within the chunk.
+                        _underlyingStream.Seek(currentChunk.Offset + 1 + decompressedOffset, SeekOrigin.Begin);
+                        currentChunk.Remainder = (int)(currentChunk.DecompressedSize - decompressedOffset);
+                    }
+
+                    currentChunk.Remainder = (int)(currentChunk.DecompressedSize - decompressedOffset);
+                    _position += (int)decompressedOffset;
+                    decompressedOffset = 0;
+
+                    _chunkIndex = i;
                 }
             }
-
-            return Position;
+            return _position;
         }
 
         private void Initialize(ref ChunkInfo chunkInfo, bool lazy = false)
@@ -292,7 +211,7 @@ namespace wowzer.fs.IO
         protected override void Dispose(bool disposing)
             => _underlyingStream.Dispose();
 
-        private record struct ChunkInfo(int CompressedSize, int DecompressedSize, UInt128 Checksum, byte CompressionMode, int Remainder);
+        private record struct ChunkInfo(int CompressedSize, int DecompressedSize, UInt128 Checksum, long Offset, byte CompressionMode, int Remainder);
         private record struct Header(int headerSize, int flags);
     }
 }
